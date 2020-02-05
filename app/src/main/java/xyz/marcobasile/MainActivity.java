a226f0f3318e67493a20e38326dcec18993cbcaf
Marco Basile <mbasile@tai.it>
2020-02-05 14:16:27 +0100

    aggiunto twitter login button

--- app/src/main/java/xyz/marcobasile/MainActivity.java
+++ app/src/main/java/xyz/marcobasile/MainActivity.java
@@ -1,11 +1,15 @@
 package xyz.marcobasile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +21,17 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView navView;
+    private TwitterCore twitterCore;
+    private TweetComposer tweetComposer;
+    private TwitterLoginButton loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Twitter.initialize(this);
 
-        TwitterCore twitterCore = TwitterCore.getInstance();
-        TweetComposer tweetComposer = TweetComposer.getInstance();
+        twitterCore = TwitterCore.getInstance();
+        tweetComposer = TweetComposer.getInstance();
 
         setupMainView();
 
@@ -33,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isUserAuthenticated() {
-        return true;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginBtn.onActivityResult(requestCode, resultCode, data);
     }
 
     private void setupMainView() {
@@ -44,13 +53,21 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_map, R.id.navigation_saved_posts)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
     private void setupLoginView() {
-        // TODO far apparire il bottone di Login
+        loginBtn = findViewById(R.id.login_button);
+        loginBtn.setVisibility(View.VISIBLE);
         navView.setVisibility(View.INVISIBLE);
+    }
+
+    private boolean isUserAuthenticated() {
+        TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        // TwitterAuthToken authToken = session.getAuthToken();
+        return null == session;
     }
 }
