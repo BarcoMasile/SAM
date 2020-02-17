fdd7f5000b1804a866c4903e1b9b7bee5d308aa5
Marco Basile <bigfootbasile@gmail.com>
2020-02-18 00:50:54 +0100

    inizio implementazione service

---
+++ app/src/main/java/xyz/marcobasile/receiver/publish/PicturePublisherReceiver.java
@@ -0,0 +1,18 @@
+/*package xyz.marcobasile.receiver.publish;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class PicturePublisherReceiver extends BroadcastReceiver {
+
+    private final String TAG = PicturePublisherReceiver.class.getName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.i(TAG, "onReceive");
+
+    }
+}*/
